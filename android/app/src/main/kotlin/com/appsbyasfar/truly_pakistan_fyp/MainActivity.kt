8a9bdc4625d2e87c6a45c0ff04dc5021cc6810ac
Asfar Ali <s4.ali@outlook.com>
2020-08-05 06:05:44 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/appsbyasfar/truly_pakistan_fyp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appsbyasfar.truly_pakistan_fyp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:truly_pakistan_fyp/providers/theme_provider.dart';
+import 'package:truly_pakistan_fyp/ui/screens/home_screen.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => ThemeProvider(),
+        )
+      ],
+      child: MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ThemeProvider>(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (_,theme,__){
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: Provider.of<ThemeProvider>(context).currentThemeData,
+            debugShowCheckedModeBanner: false,
+            home: HomeScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
